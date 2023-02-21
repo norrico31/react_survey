4cab418cef6cd308e324fef4161443e778291d0f
norrico31 <norricobiason31@gmail.com>
2023-02-21 09:18:23 +0800

    feature: axios config

--- frontend/src/axios.ts
+++ frontend/src/axios.ts
@@ -1,5 +1,21 @@
 import axios from 'axios'
+import { routes } from './routes'
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: import.meta.env.VITE_API_BASE_URL + '/api'
 })
+
+axiosClient.interceptors.request.use((config) => {
+    const token = '123'
+    config.headers.Authorization = 'Bearer ' + token
+    return config
+})
+
+axiosClient.interceptors.response.use((res) => res, err => {
+    if (err.response && err.response.status === 401) {
+        routes.navigate('/login')
+        return err
+    } throw err
+})
+
+export default axiosClient
