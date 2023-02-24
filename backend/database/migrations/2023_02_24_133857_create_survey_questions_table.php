8493d2fbba42d6304098be13d38a65151bd4af1b
norrico31 <norricobiason31@gmail.com>
2023-02-24 21:49:00 +0800

    backend migrations done

--- backend/database/migrations/2023_02_24_133857_create_survey_questions_table.php
+++ backend/database/migrations/2023_02_24_133857_create_survey_questions_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Survey;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,11 @@ return new class extends Migration
     {
         Schema::create('survey_questions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(Survey::class, 'survey_id');
+            $table->string('type', 45);
+            $table->string('question', 2000);
+            $table->string('description')->nullable();
+            $table->string('data')->nullable();
             $table->timestamps();
         });
     }
--- backend/database/migrations/2023_02_24_133914_create_survey_question__answers_table.php
+++ backend/database/migrations/2023_02_24_133914_create_survey_question__answers_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\SurveyAnswer;
+use App\Models\SurveyQuestion;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +17,9 @@ return new class extends Migration
     {
         Schema::create('survey_question__answers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId(SurveyQuestion::class, 'survey_question_id');
+            $table->foreignId(SurveyAnswer::class, 'survey_answer_id');
+            $table->text('answer');
             $table->timestamps();
         });
     }
